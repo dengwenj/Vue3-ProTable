be2ad94feb029a434e078068089ae6725ce4a826
朴睦 <927561237@qq.com>
2024-02-27 14:59:27 +0800

    项目配置

---
+++ src/components/index.d.ts
@@ -0,0 +1,6 @@
+import type { PMProTableProps } from './ProTable/types'
+import type { ProFormProps } from './ProForm/types'
+
+declare function ProTable(props: PMProTableProps): any 
+
+declare function ProForm(props: ProFormProps): any
--- vite.config.ts
+++ vite.config.ts
@@ -24,25 +24,25 @@ export default defineConfig({
   //     fileName: 'protable',
   //   },
   // }
-  build: {
-    outDir: 'package',
-    // cssCodeSplit: true, // 强制内联CSS
-    rollupOptions: {
-      // 请确保外部化那些你的库中不需要的依赖
-      external: ['vue'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-    lib: {
-      // entry: resolve(__dirname, 'packages/index.ts'),
-      entry: './src/components/index.ts',
-      name: 'protable',
-      // formats: ['es', 'cjs'],
-      fileName: 'protable',
-    },
-  }
+  // build: {
+  //   outDir: 'lib',
+  //   // cssCodeSplit: true, // 强制内联CSS
+  //   rollupOptions: {
+  //     // 请确保外部化那些你的库中不需要的依赖
+  //     external: ['vue'],
+  //     output: {
+  //       // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+  //       globals: {
+  //         vue: 'Vue',
+  //       },
+  //     },
+  //   },
+  //   lib: {
+  //     // entry: resolve(__dirname, 'packages/index.ts'),
+  //     entry: './src/components/index.ts',
+  //     name: 'protable',
+  //     // formats: ['es', 'cjs'],
+  //     fileName: 'protable',
+  //   },
+  // }
 })
